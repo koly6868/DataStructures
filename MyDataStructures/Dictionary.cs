8a80f6c2c039b0c01bcbd5d24f5025d7042ccf03
koly6868 <koly6868@users.noreply.github.com>
2019-03-16 22:12:10 +0300

    Dictionary 1.0

---
+++ MyDataStructures/Dictionary.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyDataStructures
+{
+    public class Dictionary<TKey, TElement>
+    {
+        public Dictionary(int capacity)
+        {
+            if (capacity <= 0) throw new InvalidOperationException("Capacity must be positive");
+            size_ = capacity;
+            data_ = new DateNode[size_];
+        }
+
+
+
+        public Dictionary(IEnumerable<TKey> keys, IEnumerable<TElement> elements, int? size = null)
+        {
+            if (size == null)
+            {
+                size_ = keys.Count();
+                if (size_ != elements.Count()) throw new ArgumentException("Arguments have different size");
+            }
+            else
+            {
+                size_ = size.Value;
+            }
+            data_ = new DateNode[size_];
+            IEnumerator<TKey> keysEnumerator = keys.GetEnumerator();
+            IEnumerator<TElement> elementsEnumerator = elements.GetEnumerator();
+
+            for (int i = 0; i < size_; i++)
+            {
+                elementsEnumerator.MoveNext();
+                keysEnumerator.MoveNext();
+                Add(keysEnumerator.Current, elementsEnumerator.Current);
+            }
+        }
+
+
+
+        public void Add(TKey key, TElement element)
+        {
+            int hKey = Hash(key);
+            DateNode newNode = new DateNode(key, element);
+
+            if (data_[hKey] == null)
+            {
+                data_[hKey] = newNode;
+            }
+            else
+            {
+                DateNode lastNode = data_[hKey];
+                while (lastNode.Next != null)
+                {
+                    lastNode = lastNode.Next;
+                }
+                lastNode.Next = newNode;
+            }
+        }
+
+
+
+        public void Remove(TKey key)
+        {
+            DateNode lastNode = data_[Hash(key)];
+
+            while (lastNode != null)
+            {
+                if (lastNode.Next.Key.Equals(key))
+                {
+                    lastNode.Next = lastNode.Next.Next;
+                    return;
+                }
+                else
+                {
+                    lastNode = lastNode.Next;
+                }
+            }
+
+            throw new InvalidOperationException("Element Not Found");
+        }
+
+
+
+        public TElement Get(TKey key)
+        {
+            DateNode lastNode = data_[Hash(key)];
+
+            while (lastNode != null)
+            {
+                if (lastNode.Key.Equals(key))
+                {
+                    return lastNode.Element;
+                }
+                lastNode = lastNode.Next;
+            }
+
+            throw new IndexOutOfRangeException("Not Found");
+        }
+
+
+
+        public TElement this[TKey key]
+        {
+            get { return Get(key); }
+            set { Add(key, value); }
+        }
+
+
+
+        private int Hash(TKey key)
+        {
+            return Math.Abs(key.GetHashCode() % (size_));
+        }
+
+
+
+        private int size_;
+        private DateNode[] data_;
+
+
+
+        class DateNode
+        {
+            public DateNode(TKey key, TElement element)
+            {
+                Key = key;
+                Element = element;
+                Next = null;
+            }
+
+            public TKey Key { get; }
+            public TElement Element { get; set; }
+            public DateNode Next { get; set; }
+        }
+    }
+}
---
+++ Tests/DictionaryTests.cs
@@ -0,0 +1,47 @@
+using System;
+using Xunit;
+using MyDataStructures;
+
+namespace Tests
+{
+    public class DictionaryTests
+    {
+        [Fact]
+        public void AddElement()
+        {
+            int[] answ = new int[] { 1, 2, 3, 4 };
+            string[] answKeys = new string[] { "first", "second", "third", "fourhts" };
+            Dictionary<string, int> table = new Dictionary<string, int>(answ.Length);
+
+
+            for (int i = 0; i < answ.Length; i++)
+            {
+                table.Add(answKeys[i], answ[i]);
+            }
+
+
+            for (int i = 0; i < answ.Length; i++)
+            {
+                Assert.True(table[answKeys[i]] == answ[i]);
+            }
+        }
+
+
+
+        [Fact]
+        public void RemoveElement()
+        {
+            int[] input = new int[] { 1, 2, 3, 4 };           
+            string[] inputKeys = new string[] { "first", "second", "third", "fourhts" };
+            Dictionary<string, int> table = new Dictionary<string, int>(inputKeys, input);
+            int removeEl = 3;
+            string removeKey = "third";
+
+            table.Remove(removeKey);
+            
+            Assert.Throws<IndexOutOfRangeException>(() => table[removeKey]);
+        }
+    }
+
+   
+}
